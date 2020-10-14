8aa1d4e8c1316e5d036bbfc99b8c4f3922ac7121
Gr871 <ky3971@yandex.ru>
2020-10-14 17:04:54 +0300

    Swipe control completed

--- Assets/Scripts/Core/EventManager.cs
+++ Assets/Scripts/Core/EventManager.cs
@@ -15,6 +15,7 @@ namespace Core
         TranslatedSoundLoaded,
         
         OnDateChanged,
+        BeforeMonthChanged,
         OnMonthChanged,
         OnCalendarEventTypeChanged,
         
